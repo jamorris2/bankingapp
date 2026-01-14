1977517e50715dcc84a8dcfcc2e31d22ce379389
Jason <allinforyah@gmail.com>
2026-01-14 16:59:18 +0200

    Make signup page actually send a request to the backend to create an account

--- src/main/java/com/icebank/controller/AuthController.java
+++ src/main/java/com/icebank/controller/AuthController.java
@@ -1,10 +1,20 @@
 package com.icebank.controller;
 
+import com.icebank.model.Account;
+import com.icebank.model.AccountRequestDTO;
+import com.icebank.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AuthController {
+
+    @Autowired
+    private AccountService accountService;
 
     // The "default" page
     @GetMapping("/")
@@ -18,7 +28,20 @@ public class AuthController {
     }
 
     @GetMapping("/signup")
-    public String showSignupPage() {
+    public String showSignupPage(Model model) {
+        model.addAttribute("accountRequest", new AccountRequestDTO());
         return "signup"; // This looks for signup.html
+    }
+
+    @PostMapping("/signup")
+    public String registerAccount(@ModelAttribute("accountRequest") AccountRequestDTO dto) {
+        Account account = new Account();
+        account.setName(dto.getName());
+        account.setEmail(dto.getEmail());
+
+        accountService.saveAccount(account);
+
+        // Redirect to login with a success parameter
+        return "redirect:/login?success";
     }
 }
